757b45e62a95edf7585e6c9bcc6f5a49e4043216
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2023-05-30 00:57:29 +0900

    Fix #7 by ignoring the exception

---
+++ TranslationForTheOutsider/IssueOfTheOutsiderPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationForTheOutsider {
+    [HarmonyPatch]
+    public static class IssueOfTheOutsiderPatch {
+        [HarmonyFinalizer]
+        [HarmonyPatch(typeof(NomaiTranslatorProp), nameof(NomaiTranslatorProp.UpdateTimeFreeze))]
+        public static Exception NomaiTranslatorProp_UpdateTimeFreeze_Finalizer(Exception __exception) {
+            // Deal with https://github.com/TRSasasusu/TranslationForTheOutsider/issues/7
+            return null;
+        }
+    }
+}
