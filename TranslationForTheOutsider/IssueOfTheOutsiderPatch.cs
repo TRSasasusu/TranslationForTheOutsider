2d366d4816f9f0715258b64ba3addf113c6a1550
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2023-06-11 16:45:08 +0900

    Remove unnecessary codes related to #9

--- TranslationForTheOutsider/IssueOfTheOutsiderPatch.cs
+++ TranslationForTheOutsider/IssueOfTheOutsiderPatch.cs
@@ -70,7 +70,7 @@ namespace TranslationForTheOutsider {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NomaiRemoteCameraPlatform), nameof(NomaiRemoteCameraPlatform.SwitchToRemoteCamera))]
         public static bool NomaiRemoteCameraPlatform_SwitchToRemoteCamera_Prefix() {
-            TranslationForTheOutsider.Instance.Log("SwitchToRemoteCamera now.");
+            //TranslationForTheOutsider.Instance.Log("SwitchToRemoteCamera now.");
             _projecting = true;
             return true;
         }
@@ -78,74 +78,43 @@ namespace TranslationForTheOutsider {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NomaiRemoteCameraPlatform), nameof(NomaiRemoteCameraPlatform.SwitchToPlayerCamera))]
         public static bool NomaiRemoteCameraPlatform_SwitchToPlayerCamera_Prefix() {
-            TranslationForTheOutsider.Instance.Log("SwitchToPlayerCamera now.");
+            //TranslationForTheOutsider.Instance.Log("SwitchToPlayerCamera now.");
             _projecting = false;
-            return true;
-        }
-
-        //[HarmonyPrefix]
-        //[HarmonyPatch(typeof(NomaiProjector), nameof(NomaiProjector.FadeIn))]
-        //public static bool NomaiProjector_FadeIn_Prefix() {
-        //    TranslationForTheOutsider.Instance.Log("FadeIn now."); // this is not called when setting projection stone
-        //    _projecting = true;
-        //    return true;
-        //}
-
-        //[HarmonyPrefix]
-        //[HarmonyPatch(typeof(NomaiProjector), nameof(NomaiProjector.FadeOut))]
-        //public static bool NomaiProjector_FadeOut_Prefix() {
-        //    TranslationForTheOutsider.Instance.Log("FadeOut now."); // this is not called when setting projection stone
-        //    _projecting = false;
-        //    return true;
-        //}
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(FogWarpDetector), nameof(FogWarpDetector.TrackFogWarpVolume))]
-        public static bool ForWarpDetector_TrackFogWarpVolume_Prefix() {
-            TranslationForTheOutsider.Instance.Log("TrackForWarpVolume now.");
             return true;
         }
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(FogWarpVolume), nameof(FogWarpVolume.WarpDetector))]
         public static bool FogWarpVolume_WarpDetector_Prefix() {
-            TranslationForTheOutsider.Instance.Log("Warped maybe.");
+            //TranslationForTheOutsider.Instance.Log("Warped maybe.");
+            if(!TranslationForTheOutsider.Instance.IsFixIssuesOfTheOutsider) {
+                return true;
+            }
             return !_projecting;
         }
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(FogWarpDetector), nameof(FogWarpDetector.FixedUpdate))]
         public static bool FogWarpDetector_FixedUpdate_Prefix() {
+            if(!TranslationForTheOutsider.Instance.IsFixIssuesOfTheOutsider) {
+                return true;
+            }
             return !_projecting;
-        }
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(StreamingRenderMeshHandle), nameof(StreamingRenderMeshHandle.LoadMesh))]
-        public static bool StreamingRenderMeshHandle_LoadMesh_Prefix(StreamingRenderMeshHandle __instance) {
-            if(__instance.meshIndex == 55) {
-                TranslationForTheOutsider.Instance.Log($"seed is loaded! ({__instance.name})");
-            }
-            return true;
-        }
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(StreamingRenderMeshHandle), nameof(StreamingRenderMeshHandle.UnloadMesh))]
-        public static bool StreamingRenderMeshHandle_UnloadMesh_Prefix(StreamingRenderMeshHandle __instance) {
-            if(__instance.meshIndex == 55) {
-                TranslationForTheOutsider.Instance.Log($"seed is unloaded! ({__instance.name})");
-            }
-            return true;
         }
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NomaiRemoteCameraStreaming), nameof(NomaiRemoteCameraStreaming.NomaiRemoteCameraPlatformIDToSceneName))]
         public static bool NomaiRemoteCameraStreaming_NomaiRemoteCameraPlatformIDToSceneName_Prefix(NomaiRemoteCameraPlatform.ID id, ref string __result) {
+            if(!TranslationForTheOutsider.Instance.IsFixIssuesOfTheOutsider) {
+                return true;
+            }
+
             // DarkBramble's SharedStone (i.e., projection stone) in Ash Twin Project has an id (i.e., _connectedPlatform), 100.
             // This code replace an empty string result as StreamingGroup of "DarkBramble" in L50 of NomaiRemoteCameraStreaming.cs (in its FixedUpdate)
             //   - _sceneName of StreamingGroup of DarkBramble is "DarkBramble", so it works.
             // The Outsider did not fix this function but fixed IDToPlanetString, see https://github.com/StreetlightsBehindTheTrees/Outer-Wilds-The-Outsider/blob/65d297e9e2a9e02c7ea40c72b1a109c440a0b2e0/TheOutsider/OuterWildsHandling/OWPatches.cs#L283
             if ((int)id == 100) {
-                TranslationForTheOutsider.Instance.Log("id to darkbramble.");
+                //TranslationForTheOutsider.Instance.Log("id to darkbramble.");
                 __result = "DarkBramble";
                 return false;
             }
